c84c04dae6e2a6488cb0b25d2a831f8acd6d76fd
Maleaume Berquer <mberquerpro@gmail.com>
2024-07-24 15:48:35 +0000

    api call and pin assets

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,21 @@
 import {Component} from '../core/Component.js';
 import {createElement} from '../core/DomUtils.js';
 import * as L from "https://cdn.jsdelivr.net/npm/leaflet@1.8.0/dist/leaflet-src.esm.js";
+import {fetchSites} from '../../js/Api.js';
+
+let siteData;
+
+var Icon = L.Icon.extend({
+    options: {
+        iconSize:     [70, 70],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76]
+    }
+});
+
+var phrygeIcon = new Icon({iconUrl: '../../assets/phryge.svg'});
 
 
 export class Map extends Component {
@@ -12,7 +27,7 @@ export class Map extends Component {
     componentDidMount() {
         const mapElement = document.getElementById(this.props.id);
         if (mapElement) {
-            this.map = L.map(mapElement).setView([51.505, -0.09], 13);
+            this.map = L.map(mapElement).setView([48.866669, 2.33333], 13);
 
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -29,5 +44,27 @@ export class Map extends Component {
 
     display() {
         this.componentDidMount(); // Initialiser la carte après l'affichage du composant
+        this.createPoints();
+    }
+
+    createPoints() {
+        const apiUrl = 'https://data.paris2024.org/api/explore/v2.1/catalog/datasets/paris-2024-sites-de-competition/records?select=nom_site%2C%20sports%2C%20point_geo&limit=100';
+
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(data => {
+                siteData = data.results;
+                siteData.forEach(element => {
+                    L.marker([element.point_geo.lat, element.point_geo.lon], {icon: phrygeIcon}).addTo(this.map).bindPopup(element.nom_site);
+                });
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
     }
 }
