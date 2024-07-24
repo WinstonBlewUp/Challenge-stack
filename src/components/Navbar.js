772494cb63877b8899e2098851dad7e59e6a714c
Goldenback <iliesrimani.work@gmail.com>
2024-07-25 01:34:29 +0200

    feat: list of events finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import { Component } from '../core/Component.js';
-import { createElement } from '../core/DomUtils.js';
+import {Component} from '../core/Component.js';
+import {createElement} from '../core/DomUtils.js';
 
 export class Navbar extends Component {
     constructor(props) {
@@ -11,14 +11,14 @@ export class Navbar extends Component {
     render() {
         const currentPath = window.location.pathname;
 
-        return createElement('div', { id: 'navbar-container' },
-            createElement('div', { id: 'navbar', className: `w-full fixed bottom-0 bg-white rounded-t-xl md:hidden ${this.isModalOpen ? 'no-shadow' : ''}` },
-                createElement('div', { className: `navbar-bar ${this.isModalOpen ? 'hidden' : ''}`, onclick: this.toggleModal }),
-                createElement('div', { className: 'flex justify-around items-center h-24' },
-                    this.createNavItem('Home', homeIcon, '/', currentPath),
-                    this.createNavItem('Events', eventsIcon, '/event', currentPath),
-                    this.createNavItem('Favorites', favoriteIcon, '/favorite', currentPath),
-                    this.createNavItem('My Tickets', ticketIcon, '/ticket', currentPath)
+        return createElement('div', {id: 'navbar-container'},
+            createElement('div', {id: 'navbar', className: `w-full fixed bottom-0 bg-white rounded-t-xl md:hidden ${this.isModalOpen ? 'no-shadow' : ''}`},
+                createElement('div', {className: `navbar-bar ${this.isModalOpen ? 'hidden' : ''}`, onclick: this.toggleModal}),
+                createElement('div', {className: 'flex justify-around items-center h-24'},
+                    this.createNavItem('Accueil', homeIcon, '/', currentPath),
+                    this.createNavItem('Événements', eventsIcon, '/events', currentPath),
+                    this.createNavItem('Favoris', favoriteIcon, '/favorite', currentPath),
+                    this.createNavItem('Billetterie', ticketIcon, '/ticket', currentPath)
                 )
             ),
             this.createModal()
@@ -27,19 +27,19 @@ export class Navbar extends Component {
 
     createNavItem(label, iconSvg, href, currentPath) {
         const isActive = currentPath === href;
-        const linkClassName = isActive ? 'flex flex-col items-center text-black nav-link' : 'flex flex-col items-center text-gray-600 nav-link';
-        const iconClassName = isActive ? 'h-8 mb-1 text-black' : 'h-8 mb-1';
-        return createElement('a', { href: `${href}`, className: linkClassName },
-            createElement('div', { className: iconClassName, innerHTML: iconSvg }),
-            createElement('span', { className: 'text-sm' }, label)
+        const linkClassName = isActive ? 'flex flex-col items-center text-black font-bold text-red-500 nav-link' : 'flex flex-col items-center text-gray-600 nav-link';
+        const iconClassName = isActive ? 'h-8 mb-1 text-black font-bold text-red-500' : 'h-8 mb-1';
+        return createElement('a', {href: `${href}`, className: linkClassName},
+            createElement('div', {className: iconClassName, innerHTML: iconSvg}),
+            createElement('span', {className: 'text-sm'}, label)
         );
     }
 
     createModal() {
         const modalClass = this.isModalOpen ? 'modal open' : 'modal';
-        return createElement('div', { className: modalClass },
-            createElement('div', { className: 'modal-content' },
-                createElement('div', { className: 'navbar-bar', onclick: this.toggleModal }),
+        return createElement('div', {className: modalClass},
+            createElement('div', {className: 'modal-content'},
+                createElement('div', {className: 'navbar-bar', onclick: this.toggleModal}),
                 createElement('p', {}, 'Additional content goes here')
             )
         );
@@ -54,8 +54,7 @@ export class Navbar extends Component {
         if (this.isModalOpen) {
             navbar.classList.add('no-shadow');
             navbarBar.classList.add('hidden');
-        }
-        else {
+        } else {
             navbar.classList.remove('no-shadow');
             navbarBar.classList.remove('hidden');
         }
@@ -69,9 +68,6 @@ export class Navbar extends Component {
         }
     }
 }
-
-
-
 
 
 const homeIcon = `
